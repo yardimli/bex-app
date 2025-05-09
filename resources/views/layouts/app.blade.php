dfcd4359011fbcbbb66a5bc53564c9f105fcd0a8
Ekim Emre Yardimli <ekimyardimli@izlenim.com>
2025-05-09 08:58:24 +0800

    favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,11 @@
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 	<!-- Custom CSS -->
 	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+	
+	<link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png">
+	<link rel="manifest" href="/images/site.webmanifest">
 	@stack('styles')
 </head>
 <body>
