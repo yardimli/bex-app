a2644a359731d6a998c90d24396945854175e5fd
Ekim Emre Yardimli <ekimyardimli@izlenim.com>
2025-09-27 13:39:40 +0800

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,5 +65,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
   	    'subscribed' => \App\Http\Middleware\CheckSubscription::class,
+   	    'check.subscription' => \App\Http\Middleware\CheckSubscriptionOrTeamMembership::class,
+
     ];
 }
